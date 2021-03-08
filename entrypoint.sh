1dd121ce2fe751f4be7f056b9e5d46d57e4a5f32
Alex Zuniga <alex.zuniga@wpengine.com>
2021-03-08 14:38:04 -0600

    updated fi syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,4 +44,3 @@ chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 
 # Deploy via SSH
 rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
-fi
